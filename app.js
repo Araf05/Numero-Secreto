457633aaddbb6332e0e1f590c47d728db987eba2
Araceli Fernández <126494947+Araf05@users.noreply.github.com>
2024-01-22 12:52:22 -0300

    Cantidad limitada de intentos por juego

--- app.js
+++ app.js
@@ -1,40 +1,30 @@
-/*
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un n�mero por favor:");
-
-console.log(numeroUsuario);
-
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el n�mero');
-}
-
-javascript
-alert('Bienvenidos al juego del número secreto');
-let eleccion = prompt('Elige un número entre 1 y 10');
-
-let numeroSecreto = 4;
-
-if (eleccion == numeroSecreto) {
-    alert('Adivinaste');
-} 
-
-*/
 alert("¡Bienvenida y bievenido a nuestro sitio web!");
 let numeroSecreto = 6;
 let numeroUsuario = 0;
+let intentos = 1;
+let palabraIntento = 'intento';
+let maxIntentos = 3;
 
 while(numeroUsuario != numeroSecreto)
 {
     numeroUsuario = prompt('Elige un número entre 1 y 10: ');
-    console.log(numeroUsuario);
+    console.log(numeroUsuario)
 
     if(numeroUsuario == numeroSecreto){
-        alert(`Adivinaste! el número secreto es: ${numeroUsuario}`);
+        alert(`Adivinaste! el número secreto es: ${numeroUsuario}, lo lograste en ${intentos} ${palabraIntento}`);
     } else {
         if(numeroUsuario > numeroSecreto){
             alert(`El numero secreto es menor que ${numeroUsuario}`);
         } else {
             alert(`El numero secreto es mayor que ${numeroUsuario}`);
         }
+        intentos++;
+        palabraIntento = 'intentos';
+        if(intentos > maxIntentos)
+        {
+            alert(`Gastaste tus ${maxIntentos} intetos disponibles`);
+            break;
+        }
     }
+    console.log('Cantidad de intentos: ' + intentos)
 }
