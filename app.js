60919475a2db2cb6a0641e8e4b1d6ad8d523e393
Araceli Fernández <126494947+Araf05@users.noreply.github.com>
2024-01-22 13:00:47 -0300

    Operador ternario

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ alert("¡Bienvenida y bievenido a nuestro sitio web!");
 let numeroSecreto = 6;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraIntento = 'intento';
 let maxIntentos = 3;
 
 while(numeroUsuario != numeroSecreto)
@@ -11,7 +10,7 @@ while(numeroUsuario != numeroSecreto)
     console.log(numeroUsuario)
 
     if(numeroUsuario == numeroSecreto){
-        alert(`Adivinaste! el número secreto es: ${numeroUsuario}, lo lograste en ${intentos} ${palabraIntento}`);
+        alert(`Adivinaste! el número secreto es: ${numeroUsuario}. Lo lograste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
     } else {
         if(numeroUsuario > numeroSecreto){
             alert(`El numero secreto es menor que ${numeroUsuario}`);
@@ -19,7 +18,6 @@ while(numeroUsuario != numeroSecreto)
             alert(`El numero secreto es mayor que ${numeroUsuario}`);
         }
         intentos++;
-        palabraIntento = 'intentos';
         if(intentos > maxIntentos)
         {
             alert(`Gastaste tus ${maxIntentos} intetos disponibles`);
