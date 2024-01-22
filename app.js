4369b0c85dbc4203d9fa69de7774f445225f0bb2
Araceli Fernández <126494947+Araf05@users.noreply.github.com>
2024-01-22 00:39:30 -0300

    Primer commit, practicando Js

---
+++ app.js
@@ -0,0 +1,29 @@
+/*
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un n�mero por favor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el n�mero');
+}
+
+javascript
+alert('Bienvenidos al juego del número secreto');
+let eleccion = prompt('Elige un número entre 1 y 10');
+
+let numeroSecreto = 4;
+
+if (eleccion == numeroSecreto) {
+    alert('Adivinaste');
+} 
+
+*/
+alert("¡Bienvenida y bievenido a nuestro sitio web!");
+let numeroSecreto = 6;
+let numeroUsuario = prompt('Elige un número entre 1 y 10: ');
+console.log(numeroUsuario);
+
+if(numeroUsuario == numeroSecreto){
+    alert('Adivinaste el número secreto!')
+}
